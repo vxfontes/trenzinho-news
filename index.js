bbd2de120249812f23c0c6bbee19a36abd5ebe25
vxfontes <nessa1vane@icloud.com>
2023-05-19 16:54:59 -0300

    Revert "Setup"
    This reverts commit ddce7ab58d1048fa6a335cb71305c13bc8b7e265.

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-// const routes = require('./server/routes/allRoutes')
-
-
-// app.use('/', routes);
-
-/* Definindo qual porta a aplicação vai rodar */
-app.listen(3003, () => console.log("O servidor está rodando na porta 3003"));
