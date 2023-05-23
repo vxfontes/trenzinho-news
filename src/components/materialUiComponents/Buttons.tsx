07480e72af8a78ced7c7b578a93c275ceb531b8e
vxfontes <nessa1vane@icloud.com>
2023-05-23 12:01:54 -0300

    feat: botão branco

--- src/components/materialUiComponents/Buttons.tsx
+++ src/components/materialUiComponents/Buttons.tsx
@@ -8,3 +8,11 @@ export const ColorButtonBlue = styled(Button)<ButtonProps>(({
         backgroundColor: '#2c70dc',
     },
 }));
+
+export const ColorButtonWhite = styled(Button)<ButtonProps>(({
+    color: '#5a97f7',
+    backgroundColor: '#e2e2e2',
+    '&:hover': {
+        backgroundColor: '#b3b1b1',
+    },
+}));
