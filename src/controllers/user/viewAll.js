d6c45e6d0f95ddf485f01c21ee459daa0bcf1fe8
lais-cardoso <lalaicardoso27@hotmail.com>
2023-05-23 17:22:56 -0300

    Implementação da consulta myEvents

--- src/controllers/user/viewAll.js
+++ src/controllers/user/viewAll.js
@@ -11,6 +11,30 @@ const exibirTodosUsuarios = async (req, res) => {
     }
 };
 
+/* Exibe todos os eventos selecionados por um usuário*/
+
+const myEvents = async (req, res) => {
+    const id_user = req.query.id_user;
+
+    try {
+        const consulta = `SELECT * FROM evento INNER JOIN interesse ON evento.codigo = interesse.cod_evento WHERE interesse.id_user = '${id_user}'`;
+        const resultado = await db.query(consulta);
+        res.json(resultado.rows);
+
+        if(resultado.rows == '') {
+            res.status(400).json({status: 'error', message: 'Evento não encontrado'})
+        }
+        else {
+            res.status(200).json({status: 'sucess', result: resultado.rows })
+        }
+
+    }catch(error){
+        console.log('Erro ao executar a consulta', error);
+        res.status(500).json({status: 'error', message: 'Erro ao executar a consulta'});
+    }
+}
+
 module.exports = {
-    exibirTodosUsuarios: exibirTodosUsuarios
+    exibirTodosUsuarios: exibirTodosUsuarios,
+    myEvents: myEvents
 };
--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const route = express.Router();
 
 /* Rotas de Usuário */
-const { exibirTodosUsuarios } = require('../controllers/user/viewAll');
-
+const { exibirTodosUsuarios, myEvents } = require('../controllers/user/viewAll');
 
 /* Rotas de Evento */
 const { eventosPorNome, eventosPorCodigo, eventosPorCategoria } = require('../controllers/event/research');
 const { exibirEventos, eventosEInteresses, exibirTodasCategorias } = require('../controllers/event/viewAll');
-
 
 
 /* exibição de todos os itens de uma tabela*/
@@ -17,11 +15,11 @@ route.get('/eventsInterested', eventosEInteresses); // exibir nossa tabela de ev
 route.get('/users', exibirTodosUsuarios); // exibir todos os usuarios
 route.get('/category', exibirTodasCategorias); // exibir todas as categorias
 
-
 /* busca e retorno em uma tabela */
 route.post('/researchName' , eventosPorNome); // Pesquisar evento por nome
 route.post('/researchCod' , eventosPorCodigo); // Pesquisar evento pelo seu código
 route.post('/researchCategory', eventosPorCategoria); // Pesquisar evento por categoria
+route.post('/myEvents', myEvents); //exibir todos os eventos selecionados por um usuário
 
 
 
