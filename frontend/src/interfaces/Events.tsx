9351f9f806b24de14b32f0d142b3f27a45b854e3
vxfontes <nessa1vane@icloud.com>
2023-05-27 11:23:18 -0300

    feat: interface de eventos

---
+++ frontend/src/interfaces/Events.tsx
@@ -0,0 +1,19 @@
+export interface EventData {
+    carga_horaria: number;
+    certificado: boolean;
+    cod_area_atuacao: number;
+    cod_categoria: number;
+    cod_event: number;
+    cod_modalidade: number;
+    data: string;
+    descricao: string;
+    horario: string;
+    link: string;
+    local: string;
+    nome_area_ataucao: string;
+    nome_categ: string;
+    nome_evento: string;
+    nome_modali: string;
+    total_interessados: string;
+    vagas: number;
+}
