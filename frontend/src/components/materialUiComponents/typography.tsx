d3a3360b70cb48c732331ff54d09e43dcc3059a6
vxfontes <nessa1vane@icloud.com>
2023-05-28 11:27:58 -0300

    feat: nova fonte

--- frontend/src/components/materialUiComponents/typography.tsx
+++ frontend/src/components/materialUiComponents/typography.tsx
@@ -3,3 +3,11 @@ import { styled, Typography } from "@mui/material";
 export const TypoBebas = styled(Typography)({
     fontFamily: ['Bebas Neue', 'cursive'].join(',')
 })
+
+export const TypoRoboto = styled(Typography)({
+    fontFamily: ['Roboto Mono', 'cursive'].join(',')
+})
+
+export const TypoNunito = styled(Typography)({
+    fontFamily: 'Nunito'
+})
