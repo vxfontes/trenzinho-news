4bf9a3d1147a40576cd2646d5fc772835239426f
vxfontes <nessa1vane@icloud.com>
2023-06-01 13:11:41 -0300

    fix: alteração da localização da navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 import RoutesApp from './routes';
 
 function App() {
 	return (
 		<Router>
-				<RoutesApp />
+			<Navbar />
+			<RoutesApp />
 		</Router>
 	)
 }
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,12 +1,10 @@
 import BackgroundTriangle from "../../components/background/backgroundIMG";
-import Navbar from "../../components/navbar/Navbar";
 import ExibicaoDeEventos from "./eventos";
 
 const Home = () => {
     return (
         <BackgroundTriangle>
 
-            <Navbar />
             <ExibicaoDeEventos />
 
         </BackgroundTriangle>
